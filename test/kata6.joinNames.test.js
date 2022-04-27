62d8eb0fe6db3893ef55423ce235f5b0c2addc61
Emma Wenham <ewenham3@gmail.com>
2022-04-27 20:17:47 +0100

    joinNames completed

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -2,6 +2,7 @@ const { joinNames } = require("../src");
 
 describe("joinNames", () => {
   test("returns string of names, seperated by commas and an ampersand", () => {
-    expect(joinNames(["Bart", "Lisa", "Maggie"])).toBe('Bart, Lisa & Maggie');
+    expect(joinNames(["Bart", "Lisa", "Maggie"])).toBe('Bart, Lisa & Maggie')
+    expect(joinNames(["Bart", "Lisa", "Maggie", "Tony"])).toEqual('Bart, Lisa, Maggie & Tony')
   });
 });
