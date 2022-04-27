8f3ca61943da06b111d270902c52b9aa854b0c81
Emma Wenham <ewenham3@gmail.com>
2022-04-27 20:30:15 +0100

    getEmployerRole completed

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,14 @@
-// const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+  if(!employees) {
+      return 'Not an employee';
+  }  
+const foundEmployee = employees.find(
+    element => element.name === employeeName);
+    if(!foundEmployee) {
+        return 'Does not work here';
+    }
 
-// module.exports = getEmployerRole;
+    return foundEmployee.role;
+};
+
+module.exports = getEmployerRole;
